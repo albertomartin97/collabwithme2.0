cb80920abfe36d62c1ad5934655366edf180586d
Alberto <bodyboarddinamic@gmail.com>
2020-02-01 09:44:20 +0000

    WELCOME SCREEN

--- app/src/main/java/com/example/collabwithme20/MainActivity.kt
+++ app/src/main/java/com/example/collabwithme20/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.collabwithme20
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.VideoView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+        val videoView = findViewById<VideoView>(R.id.videoView)
+        val path = "android.resource://" + packageName + "/" + R.raw.djstudio_vertical_zoom
+        videoView?.setVideoURI(Uri.parse(path))
+        videoView.start()
+
     }
 }
