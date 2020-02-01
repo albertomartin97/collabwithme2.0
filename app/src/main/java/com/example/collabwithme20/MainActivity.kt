ae0a4b38b417e676832dc68641f2d0038131cb9d
Alberto <bodyboarddinamic@gmail.com>
2020-02-01 10:00:22 +0000

    REGISTRATION SCREEN

--- app/src/main/java/com/example/collabwithme20/MainActivity.kt
+++ app/src/main/java/com/example/collabwithme20/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.example.collabwithme20
 
+import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.VideoView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        registrationLinkTextView.setOnClickListener {
+            val intentGoToRegistrationActivity = Intent(this, RegistrationActivity::class.java)
+            startActivity(intentGoToRegistrationActivity)
+
+        }
+
 
         val videoView = findViewById<VideoView>(R.id.videoView)
         val path = "android.resource://" + packageName + "/" + R.raw.djstudio_vertical_zoom
