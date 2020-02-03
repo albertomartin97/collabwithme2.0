aafae2f564a806a5b4a5516282a07dcf5453395a
Alberto <bodyboarddinamic@gmail.com>
2020-02-03 20:12:04 +0000

    HOMEPAGE DESIGN

---
+++ app/src/main/java/com/example/collabwithme20/HomeScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.example.collabwithme20
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_home_screen.*
+
+class HomeScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen)
+
+
+      // val appText = appTextView as TextView
+      // appText.setShadowLayer(1.5f, 1f, 1f, Color.BLACK)
+
+
+    }
+}
--- app/src/main/java/com/example/collabwithme20/LoginActivity.kt
+++ app/src/main/java/com/example/collabwithme20/LoginActivity.kt
@@ -1,12 +1,21 @@
 package com.example.collabwithme20
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        loginBtn.setOnClickListener {
+            val intentGoToHomeScreenActivity = Intent(this, HomeScreenActivity::class.java)
+            startActivity(intentGoToHomeScreenActivity)
+        }
     }
+
+
 }
