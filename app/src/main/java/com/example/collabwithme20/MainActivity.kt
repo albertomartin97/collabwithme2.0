04b377f4d7597c8b19caf74f092f1bbdbd66bd9d
Alberto <bodyboarddinamic@gmail.com>
2020-02-03 16:48:03 +0000

    BUTTONS AND ICONS UPDATED

--- app/src/main/java/com/example/collabwithme20/MainActivity.kt
+++ app/src/main/java/com/example/collabwithme20/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        registrationLinkTextView.setOnClickListener {
+        loginTextView.setOnClickListener {
             val intentGoToRegistrationActivity = Intent(this, RegistrationActivity::class.java)
             startActivity(intentGoToRegistrationActivity)
 
