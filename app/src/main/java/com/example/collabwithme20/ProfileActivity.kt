adbc509c56789b1c238d04f0385fc6b839c3fbf6
Alberto <bodyboarddinamic@gmail.com>
2020-02-11 14:14:43 +0000

    SKILLS SAVED IN DB

--- app/src/main/java/com/example/collabwithme20/ProfileActivity.kt
+++ app/src/main/java/com/example/collabwithme20/ProfileActivity.kt
@@ -6,9 +6,13 @@ import android.os.Bundle
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
 import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
+    companion object {
+        private val TAG = "ProfileActivity"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +23,52 @@ class ProfileActivity : AppCompatActivity() {
             startActivity(intentGoToPreviousActivity)
         }
 
+        getUserData() //Get user data from DB
+
+
+        musicProductionBtn.setOnClickListener {
+
+            updateSkills("music_production")
+
+        }
+
+        singerBtn.setOnClickListener {
+
+            updateSkills("singing")
+        }
+
+        rapperBtn.setOnClickListener {
+
+            updateSkills("rapping")
+
+        }
+
+        videoProductionBtn.setOnClickListener {
+
+            updateSkills("video_production")
+
+        }
+
+        graphicDesignerBtn.setOnClickListener {
+
+            updateSkills("graphic_design")
+
+        }
+
+        clothingDesignBtn.setOnClickListener {
+
+            updateSkills("clothing_design")
+
+        }
+
+        othersBtn.setOnClickListener {
+
+            updateSkills("others")
+
+        }
+    }
+
+    private fun getUserData(){
         val userName = usernameTextView
         val firstName = firstNameTextView
         val lastName = lastNameTextView
@@ -27,6 +77,7 @@ class ProfileActivity : AppCompatActivity() {
         val db = FirebaseFirestore.getInstance()
         val uid = FirebaseAuth.getInstance().currentUser?.uid ?: String()
         val docRef = db.collection("users").document(uid)
+
         docRef.get().addOnSuccessListener { document ->
             if (document != null) {
                 Log.d("exists", "DocumentSnapshot data: ${document.data}")
@@ -41,6 +92,31 @@ class ProfileActivity : AppCompatActivity() {
             .addOnFailureListener { exception ->
                 Log.d("error db", "get failed with ", exception)
             }
+    }
+
+    private fun updateSkills(skill : String){
+
+            val db = FirebaseFirestore.getInstance()
+            val uid = FirebaseAuth.getInstance().currentUser?.uid ?: String()
+
+
+            val documentRef = db.collection("users").document(uid).
+                collection("skills").document(skill)
+
+            val skillBool = "true"
+            val user = hashMapOf(
+                "skill" to skillBool
+            )
+
+            documentRef.set(user, SetOptions.merge()).addOnSuccessListener {
+                Log.d(TAG, "User profile created for $uid")
+            }
+                .addOnFailureListener { e ->
+                    Log.w(TAG, "Error adding document", e)
+                }
+
+
+
 
     }
 }
